dfa612f75e8fac3c0ea2805468794f91984a246c
Di-Mo-De-OH <dhelahep0115@gmail.com>
2026-05-26 15:39:52 +0900

    style: black 및 isort 포맷팅 적용

--- apps/core/models.py
+++ apps/core/models.py
@@ -1,8 +1,12 @@
 from django.db import models
+
 from apps.core.utils.ulid import generate_ulid
 
+
 class BaseModel(models.Model):
-    id = models.CharField(primary_key=True,max_length = 26,default = generate_ulid,editable = False)
+    id = models.CharField(
+        primary_key=True, max_length=26, default=generate_ulid, editable=False
+    )
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
