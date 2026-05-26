6bd042a8fc47d0f5dbe4437b229b756e42ad7bc6
Di-Mo-De-OH <dhelahep0115@gmail.com>
2026-05-26 13:04:28 +0900

    chore: 공용 예외처리 및 유틸 함수 추가

--- config/settings/base.py
+++ config/settings/base.py
@@ -104,6 +104,7 @@ REST_FRAMEWORK = {
         "rest_framework_simplejwt.authentication.JWTAuthentication",
     ),
     "DEFAULT_SCHEMA_CLASS": "drf_spectacular.openapi.AutoSchema",
+    "EXCEPTION_HANDLER": "apps.core.exceptions.exception_handler.custom_exception_handler",
 }
 
 SPECTACULAR_SETTINGS = {
