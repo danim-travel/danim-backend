ea6d0116367e48b57464f5302f809e93ce00a2d9
Di-Mo-De-OH <dhelahep0115@gmail.com>
2026-05-26 13:18:09 +0900

    style: black 및 isort 포맷팅 적용

--- apps/core/exceptions/exception.py
+++ apps/core/exceptions/exception.py
@@ -1,27 +1,32 @@
-from rest_framework.exceptions import APIException
 from rest_framework import status
+from rest_framework.exceptions import APIException
+
 
 class BaseCustomException(APIException):
-    status_code :int
-    default_detail :str
+    status_code: int
+    default_detail: str
+
 
 class ValidationException(BaseCustomException):
     status_code = status.HTTP_400_BAD_REQUEST
     default_detail = "잘못된 요청입니다."
 
+
 class NotFoundException(BaseCustomException):
     status_code = status.HTTP_404_NOT_FOUND
     default_detail = "존재하지 않습니다."
+
 
 class UnauthorizedException(BaseCustomException):
     status_code = status.HTTP_401_UNAUTHORIZED
     default_detail = "인증이 필요합니다."
 
+
 class ConflictException(BaseCustomException):
     status_code = status.HTTP_409_CONFLICT
     default_detail = "이미 존재합니다."
 
+
 class ForbiddenException(BaseCustomException):
     status_code = status.HTTP_403_FORBIDDEN
     default_detail = "접근 권한이 없습니다."
-
--- apps/core/utils/base62.py
+++ apps/core/utils/base62.py
@@ -5,6 +5,7 @@ def generate_6digits_safe():
     """6자리 인증코드 발급을 위한 함수"""
     return f"{secrets.randbelow(1000000):06d}"
 
+
 def generate_token():
     """sms_token 및 email_token 발급을 위한 함수"""
     return secrets.token_urlsafe(32)
