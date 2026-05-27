98e738317d6083482d6c53318ff2850f1c0b09c4
Di-Mo-De-OH <dhelahep0115@gmail.com>
2026-05-27 13:00:04 +0900

    fix:불필요한 swagger url 중복 제거

--- config/urls.py
+++ config/urls.py
@@ -13,12 +13,6 @@ urlpatterns = [
     path("admin/", admin.site.urls),
     path("api/v1/users/", include("apps.users.urls", namespace="users")),
     path("hello/", hello),
-    path("api/schema/", SpectacularAPIView.as_view(), name="schema"),
-    path(
-        "api/schema/swagger-ui/",
-        SpectacularSwaggerView.as_view(url_name="schema"),
-        name="swagger-ui",
-    ),
 ]
 
 if settings.DEBUG:
