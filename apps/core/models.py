91acb0982c1107026045475813f4ad608fcc5fa7
Di-Mo-De-OH <dhelahep0115@gmail.com>
2026-05-29 13:05:14 +0900

    style: 포멧팅 실행

--- apps/core/models.py
+++ apps/core/models.py
@@ -5,6 +5,7 @@ from apps.core.utils.ulid import generate_ulid
 
 class BaseModel(models.Model):
     """updated_at이 필요없는 경우 상속받아 사용"""
+
     id = models.CharField(
         primary_key=True, max_length=26, default=generate_ulid, editable=False
     )
@@ -17,6 +18,7 @@ class BaseModel(models.Model):
 
 class TimeStampModel(BaseModel):
     """updated_at이 필요한 경우 상속받아사용"""
+
     updated_at = models.DateTimeField(auto_now=True)
 
     class Meta:
