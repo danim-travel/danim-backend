637e7a15a23b4637cf10b3ddbefe49a721d6db45
Di-Mo-De-OH <dhelahep0115@gmail.com>
2026-05-29 10:33:15 +0900

    docs: exception 사용 예시 주석 추가

--- apps/core/exceptions/exception.py
+++ apps/core/exceptions/exception.py
@@ -17,14 +17,13 @@ raise NotFoundException("커스텀 메시지") → 커스텀 메시지
   def get(self, request):
       try:
           user = self.service.get_user(request.user.id)
-      except BaseCustomException as e: 또는 ----> except NotFoundException as e:
+      except NotFoundException as e:
           return Response({"error_detail": e.message}, status=e.status_code)
       return Response(UserSerializer(user).data)
 """
 
 
 class BaseCustomException(Exception):
-    status_code: int
     default_message: str
 
     def __init__(self, message: str | None = None) -> None:
@@ -33,25 +32,20 @@ class BaseCustomException(Exception):
 
 
 class ValidationException(BaseCustomException):
-    status_code = 400
     default_message = "잘못된 요청입니다."
 
 
 class NotFoundException(BaseCustomException):
-    status_code = 404
     default_message = "존재하지 않습니다."
 
 
 class UnauthorizedException(BaseCustomException):
-    status_code = 401
     default_message = "인증이 필요합니다."
 
 
 class ConflictException(BaseCustomException):
-    status_code = 409
     default_message = "이미 존재합니다."
 
 
 class ForbiddenException(BaseCustomException):
-    status_code = 403
     default_message = "접근 권한이 없습니다."
