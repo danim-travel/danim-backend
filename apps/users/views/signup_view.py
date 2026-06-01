631d2a90f5e0ea747468d7e4ec70785c51ea5799
Di-Mo-De-OH <dhelahep0115@gmail.com>
2026-06-01 15:58:33 +0900

    feat:유저 회원가입 이메일 인증 로직 구현

--- apps/users/views/signup_view.py
+++ apps/users/views/signup_view.py
@@ -3,24 +3,30 @@ from rest_framework.permissions import AllowAny
 from rest_framework.request import Request
 from rest_framework.response import Response
 from rest_framework.views import APIView
-from apps.users.services.signup_service import SignUpService
+
 from apps.users.serializers.signup_serializer import UserSignUpSerializer
+from apps.users.services.signup_service import SignUpService
+
 
 class SignupView(APIView):
     """
     POST api/v1/users/signup
     유저 회원가입에 관한 class
     """
+
     permission_classes = [AllowAny]
     service = SignUpService()
 
-    def post(self,request:Request)->Response:
+    def post(self, request: Request) -> Response:
         """
         user로 부터 data를 받아 유저를 생성하는 view
         """
         serializer = UserSignUpSerializer(data=request.data)
         serializer.is_valid(raise_exception=True)
         self.service.create_user(serializer.validated_data)
-        return Response({
-            "detail":"회원가입이 완료되었습니다.",
-        },status=status.HTTP_201_CREATED,)
+        return Response(
+            {
+                "detail": "회원가입이 완료되었습니다.",
+            },
+            status=status.HTTP_201_CREATED,
+        )
