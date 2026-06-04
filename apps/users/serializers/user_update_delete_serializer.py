827a631ea70acb62c7acdeb1fdb49a019729090c
Di-Mo-De-OH <dhelahep0115@gmail.com>
2026-06-04 14:56:03 +0900

    feat: user update serializer 구현

---
+++ apps/users/serializers/user_update_delete_serializer.py
@@ -0,0 +1,29 @@
+import re
+from rest_framework import serializers
+from rest_framework.exceptions import ValidationError
+
+from apps.core.storage.s3 import s3_svc
+
+class UserUpdatedRequestSerializer(serializers.Serializer):
+    nickname = serializers.CharField(max_length=20,required=False)
+    intro = serializers.CharField(max_length=100,required=False,allow_blank=True)
+    key = serializers.CharField(max_length=255,required=False)
+
+    # TODO: check_nickname 머지 후 validators.validate_nickname_format로 교체
+    def validate_nickname(self, value: str) -> str:
+        if not re.match(r"^[a-zA-Z0-9_]+$", value):
+            raise ValidationError("닉네임은 영문, 숫자, 언더바(_)만 사용가능합니다.")
+        return value
+
+class UserUpdatedResponseSerializer(serializers.Serializer):
+    nickname = serializers.CharField()
+    intro = serializers.CharField(allow_null=True)
+    profile_img = serializers.SerializerMethodField()
+
+    # TODO: presigned 머지 후 obj.profile_img_url 프로퍼티로 교체
+    def get_profile_img(self, obj) -> str | None:
+        if not obj.profile_img:
+            return None
+        return s3_svc.create_img_url(obj.profile_img)
+
+
