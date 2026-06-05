83466dd43eb5ac0f85f692e54cde5288bac06e06
Di-Mo-De-OH <dhelahep0115@gmail.com>
2026-06-05 15:12:56 +0900

    docs: presigned-url swagger 스키마 추가

--- apps/users/schemas/__init__.py
+++ apps/users/schemas/__init__.py
@@ -3,6 +3,7 @@ from apps.users.schemas.email_view_schema import (
     email_verify_schema,
 )
 from apps.users.schemas.login_logout_schema import login_schema, logout_schema
+from apps.users.schemas.presigned_schema import user_profile_presigned_schema
 from apps.users.schemas.signup_schema import signup_schema
 from apps.users.schemas.token_schema import token_refresh_schema
 
@@ -13,4 +14,5 @@ __all__ = [
     "logout_schema",
     "signup_schema",
     "token_refresh_schema",
+    "user_profile_presigned_schema",
 ]
--- apps/users/views/presigned_url_view.py
+++ apps/users/views/presigned_url_view.py
@@ -4,8 +4,10 @@ from rest_framework.permissions import IsAuthenticated
 
 from apps.core.storage.s3 import PresignedUrlView
 from apps.core.storage.s3.services import ActionEnum, CategoryEnum, SuffixEnum
+from apps.users.schemas import user_profile_presigned_schema
 
 
+@user_profile_presigned_schema
 class UserProfileImgView(PresignedUrlView):
     permission_classes: list[type[Any]] = [IsAuthenticated]
     action = ActionEnum.UPLOAD
