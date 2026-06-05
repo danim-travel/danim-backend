f404c93bbbcba57a1733459614bb1ba788d29033
Di-Mo-De-OH <dhelahep0115@gmail.com>
2026-06-05 17:56:53 +0900

    docs: 유저 검색 swagger 문서 추가

--- apps/users/views/user_search_view.py
+++ apps/users/views/user_search_view.py
@@ -4,6 +4,7 @@ from rest_framework.response import Response
 from rest_framework.views import APIView
 
 from apps.core.utils.pagination import paginate
+from apps.users.schemas import user_search_schema
 from apps.users.serializers.user_search_serializer import UserSearchResponseSerializer
 from apps.users.services.user_search_service import UserSearchService
 
@@ -12,6 +13,7 @@ class UserSearchView(APIView):
     permission_classes = [IsAuthenticated]
     service = UserSearchService()
 
+    @user_search_schema
     def get(self, request: Request) -> Response:
         search = request.query_params.get("search", "")
         queryset = self.service.search_users(search=search)
