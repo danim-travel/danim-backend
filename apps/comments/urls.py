8921b5a293903baa9b6d1c6b3a268cef3aa34696
gunung-kim <unge001@naver.com>
2026-06-06 16:34:26 +0900

    feat : comment presigned_url view 작성 및 url 등록
    # Conflicts:
    #	apps/comments/views/views.py

--- apps/comments/urls.py
+++ apps/comments/urls.py
@@ -1,10 +1,11 @@
 from django.urls import URLPattern, path
 
-from apps.comments.views import CommentDetailView, CommentView
+from apps.comments.views import CommentDetailView, CommentPresignedURLView, CommentView
 
 app_name = "comments"
 
 urlpatterns: list[URLPattern] = [
     path("", CommentView.as_view(), name="Comments"),
     path("/<str:comment_id>", CommentDetailView.as_view(), name="CommentDetail"),
+    path("/presigned_url", CommentPresignedURLView.as_view(), name="PresignedUrl"),
 ]
--- apps/comments/views/views.py
+++ apps/comments/views/views.py
@@ -3,6 +3,7 @@ from rest_framework.permissions import IsAuthenticated, IsAuthenticatedOrReadOnl
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
+from apps.comments.pagination import CommentPagination
 from apps.comments.schemas import (
     comment_create_schema,
     comment_delete_schema,
@@ -23,6 +24,7 @@ from apps.comments.services import (
     update_comment,
 )
 from apps.core.utils.pagination import paginate
+from apps.core.storage.s3 import ActionEnum, CategoryEnum, PresignedUrlView, SuffixEnum
 
 
 class CommentView(APIView):
@@ -63,3 +65,11 @@ class CommentDetailView(APIView):
     def delete(self, request, comment_id):
         delete_comment(comment_id, request.user)
         return Response(status=status.HTTP_204_NO_CONTENT)
+
+
+class CommentPresignedURLView(PresignedUrlView):
+    permission_classes = [IsAuthenticated]
+
+    action = ActionEnum.UPLOAD
+    category = CategoryEnum.COMMENT
+    suffix = SuffixEnum.NONE
