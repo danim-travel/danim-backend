9f37803c04324bba257ae08ea9178cd40b01d5ae
gunung-kim <unge001@naver.com>
2026-06-06 17:16:48 +0900

    fix : comments urls 순서 수정

--- apps/comments/urls.py
+++ apps/comments/urls.py
@@ -6,6 +6,6 @@ app_name = "comments"
 
 urlpatterns: list[URLPattern] = [
     path("", CommentView.as_view(), name="Comments"),
-    path("/<str:comment_id>", CommentDetailView.as_view(), name="CommentDetail"),
     path("/presigned_url", CommentPresignedURLView.as_view(), name="PresignedUrl"),
+    path("/<str:comment_id>", CommentDetailView.as_view(), name="CommentDetail"),
 ]
