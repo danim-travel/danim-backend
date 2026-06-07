76ec86a8766fcc699893a10a56864fb63d84e939
Di-Mo-De-OH <dhelahep0115@gmail.com>
2026-06-07 16:28:53 +0900

    docs: 유저 업데이트 스키마 추가

--- apps/users/schemas/__init__.py
+++ apps/users/schemas/__init__.py
@@ -6,6 +6,7 @@ from apps.users.schemas.login_logout_schema import login_schema, logout_schema
 from apps.users.schemas.presigned_schema import user_profile_presigned_schema
 from apps.users.schemas.signup_schema import signup_schema
 from apps.users.schemas.token_schema import token_refresh_schema
+from apps.users.schemas.update_delete_schema import user_update_schema
 
 __all__ = [
     "email_send_schema",
@@ -15,4 +16,5 @@ __all__ = [
     "signup_schema",
     "token_refresh_schema",
     "user_profile_presigned_schema",
+    "user_update_schema",
 ]
