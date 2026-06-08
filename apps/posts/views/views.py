e389d8f767e81da3fd9448561587fa49c7312fcd
gunung-kim <unge001@naver.com>
2026-06-08 16:47:13 +0900

    feat : post 생성 api view 작성

---
+++ apps/posts/views/views.py
@@ -0,0 +1,16 @@
+from rest_framework import status
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
+from rest_framework.views import APIView
+
+
+class PostView(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def post(self, request):
+        serializer = PostCreateRequestSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        create_post(serializer.validated_data, request.user)
+        return Response(
+            {"detail": "게시글이 생성되었습니다."}, status=status.HTTP_201_CREATED
+        )
