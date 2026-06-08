46421c1e8f0944c636a7738867774e36bd490f03
gunung-kim <unge001@naver.com>
2026-06-08 18:05:22 +0900

    chore : extend_schema tags,summury 추가 및 메세지 수정

--- apps/posts/schemas/post_schemas.py
+++ apps/posts/schemas/post_schemas.py
@@ -5,8 +5,10 @@ from apps.posts.serializers import PostCreateRequestSerializer
 post_create_schema = extend_schema(
     request=PostCreateRequestSerializer,
     responses={
-        201: OpenApiResponse(description="게시글이 생성되었습니다."),
+        201: OpenApiResponse(description="게시글이 작성되었습니다."),
         400: OpenApiResponse(description="이 필드는 필수 항목입니다."),
         401: OpenApiResponse(description="인증되지 않은 사용자입니다."),
     },
+    tags=["posts"],
+    summary="게시글 생성",
 )
