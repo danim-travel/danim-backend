a0a3542f7f857308edd2a5ecd571db73be41306d
gunung-kim <unge001@naver.com>
2026-06-09 15:51:41 +0900

    feat : 팔로우 api view 작성

--- apps/follows/views/views.py
+++ apps/follows/views/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from rest_framework import status
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
+from rest_framework.views import APIView
 
-# Create your views here.
+
+class FollowCreateView(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def post(self, request, user_id):
+        result = create_follow(user_id, request.user)
+        return Response(
+            FollowResponseSerializer(result).data, status=status.HTTP_201_CREAETE
+        )
