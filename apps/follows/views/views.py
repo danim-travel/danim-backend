4f51a42a00ff98852d001a20f0b447b78c751124
gunung-kim <unge001@naver.com>
2026-06-09 16:18:43 +0900

    fix :view 코드 수정(serializer 삭제,result 응답)

--- apps/follows/views/views.py
+++ apps/follows/views/views.py
@@ -3,12 +3,12 @@ from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
+from apps.follows.services import create_follow
+
 
 class FollowCreateView(APIView):
     permission_classes = [IsAuthenticated]
 
     def post(self, request, user_id):
         result = create_follow(user_id, request.user)
-        return Response(
-            FollowResponseSerializer(result).data, status=status.HTTP_201_CREAETE
-        )
+        return Response(result, status=status.HTTP_201_CREATED)
