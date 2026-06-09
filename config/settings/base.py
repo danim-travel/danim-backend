225e858f2d74dd7bf9381d2ed0ccf06fa7ae1e91
kochanyeol <cksdufqqpu@gmail.com>
2026-06-09 18:21:29 +0900

    feat : CREDENTIALS추가 및 미들웨어 중복 제거

--- config/settings/base.py
+++ config/settings/base.py
@@ -46,7 +46,6 @@ INSTALLED_APPS = DJANGO_APPS + THIRD_APPS + OWN_APPS
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
-    "corsheaders.middleware.CorsMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
     "django.contrib.auth.middleware.AuthenticationMiddleware",
