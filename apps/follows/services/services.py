2bc109decd2ee6179ca47cb0aa01e8c2e4a1e651
gunung-kim <unge001@naver.com>
2026-06-09 16:15:47 +0900

    fix : service 코드 수정 (follower_count 반환 -> result반환)

--- apps/follows/services/services.py
+++ apps/follows/services/services.py
@@ -7,13 +7,14 @@ def create_follow(target_user_id, request_user):
     if not User.objects.filter(id=target_user_id).exists():
         raise NotFoundException("해당 유저를 찾을 수 없습니다.")
 
-    _, is_already = Follows.objects.get_or_create(
-        follower_id=target_user_id, following_id=request_user.id
+    _, created = Follows.objects.get_or_create(
+        following_id=target_user_id, follower_id=request_user.id
     )
 
-    if not is_already:
+    if not created:
         raise ConflictException("이미 팔로잉 중입니다.")
 
-    follow_count = Follows.objects.filter(follower_id=target_user_id).count()
+    follow_count = Follows.objects.filter(following_id=target_user_id).count()
 
-    return follow_count
+    result = {"follower_count": follow_count, "is_followed": created}
+    return result
