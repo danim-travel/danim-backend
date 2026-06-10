1e249ef72a61a2c76a60c27d98b1bf6fd0b43a2a
gunung-kim <unge001@naver.com>
2026-06-10 18:06:21 +0900

    test : 테스트 코드 추가(팔로우 취소 api view 404)

--- tests/test_follows/test_view/test_delete_view.py
+++ tests/test_follows/test_view/test_delete_view.py
@@ -19,3 +19,9 @@ class TestFollowDeleteView(FollowBaseTest):
         self.client.force_authenticate(user=None)
         response = self.client.delete(self.url_following_user_2)
         self.assertEqual(response.status_code, 401)
+
+    def test_non_user_delete_follow(self):
+        """로그인 한 유저의 팔로우 기록이 없는 유저 팔로우 취소 시도 view 실패 테스트"""
+        self.client.force_authenticate(user=self.user_3)
+        response = self.client.delete(self.url_following_user_2)
+        self.assertEqual(response.status_code, 404)
