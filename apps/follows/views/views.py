44dd4edb0111647f49134bf8524344b1f19044a8
gunung-kim <unge001@naver.com>
2026-06-10 14:57:38 +0900

    docs : extend_schema view 적용

--- apps/follows/views/views.py
+++ apps/follows/views/views.py
@@ -3,12 +3,14 @@ from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
+from apps.follows.schemas import follow_create_schema
 from apps.follows.services import create_follow
 
 
 class FollowCreateView(APIView):
     permission_classes = [IsAuthenticated]
 
+    @follow_create_schema
     def post(self, request, user_id):
         result = create_follow(user_id, request.user)
         return Response(result, status=status.HTTP_201_CREATED)
