d03ed4f711f8ea9634c86ccbeb1e904a3be72059
gunung-kim <unge001@naver.com>
2026-06-10 15:01:57 +0900

    docs : extend_schema 예외처리 메세지 추가

--- apps/follows/schemas/follow_schemas.py
+++ apps/follows/schemas/follow_schemas.py
@@ -10,6 +10,9 @@ follow_create_schema = extend_schema(
                     "follower_count": {"type": "integer"},
                 },
             }
-        )
+        ),
+        401: OpenApiResponse(description="로그인이 필요합니다."),
+        404: OpenApiResponse(description="해당 유저를 찾을 수 없습니다."),
+        409: OpenApiResponse(description="이미 팔로우 중입니다."),
     }
 )
