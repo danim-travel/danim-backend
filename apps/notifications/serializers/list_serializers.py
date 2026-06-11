ef83aff1abbc4fe9e8f1f9d03fe2e9b2fd19c992
gunung-kim <unge001@naver.com>
2026-06-11 17:37:58 +0900

    fix : serializer sender 필드 변경(id -> user_id),notifications_type필드 추가
    # Conflicts:
    #	config/urls.py

--- apps/notifications/serializers/list_serializers.py
+++ apps/notifications/serializers/list_serializers.py
@@ -16,18 +16,19 @@ class NotificationListSerializer(serializers.ModelSerializer):
             "target_id",
             "target_type",
             "is_read",
+            "notification_type",
             "sender",
         ]
 
     def get_sender(self, obj):
         if not obj.sender:
             return {
-                "id": None,
+                "user_id": None,
                 "nickname": "탈퇴한 유저",
                 "profile_img": None,
             }
         return {
-            "id": obj.sender.id,
+            "user_id": obj.sender.id,
             "nickname": obj.sender.nickname,
             "profile_img": obj.sender.profile_img_url,
         }
