c4db8f42d1018e59edeea525847a617943482f2d
Di-Mo-De-OH <dhelahep0115@gmail.com>
2026-06-11 10:55:46 +0900

    feat: post presigned-url 기능 구현

--- apps/posts/urls.py
+++ apps/posts/urls.py
@@ -1,10 +1,15 @@
 from django.urls import URLPattern, path
 
-from apps.posts.views import create_view, main_list_view
+from apps.posts.views import create_view, main_list_view, presigned_url_post_view
 
 app_name = "posts"
 
 urlpatterns: list[URLPattern] = [
     path("", create_view.PostCreateView.as_view(), name="post_create"),
     path("/main", main_list_view.PostMainListView.as_view(), name="post_main_list"),
+    path(
+        "/presigned-url",
+        presigned_url_post_view.PostImageView.as_view(),
+        name="presigned_url_post",
+    ),
 ]
