e80396125d4502eb958a609c290e951a9962af3a
gunung-kim <unge001@naver.com>
2026-06-11 17:12:34 +0900

    feat : notification 알림 목록조회 serialzier 작성

---
+++ apps/notifications/serializers/list_serializers.py
@@ -0,0 +1,33 @@
+from rest_framework import serializers
+
+from apps.notifications.models import Notification
+
+
+class NotificationListSerializer(serializers.ModelSerializer):
+    notification_id = serializers.IntegerField(source="id")
+    sender = serializers.SerializerMethodField()
+
+    class Meta:
+        model = Notification
+        fields = [
+            "notification_id",
+            "message",
+            "created_at",
+            "target_id",
+            "target_type",
+            "is_read",
+            "sender",
+        ]
+
+    def get_sender(self, obj):
+        if not obj.sender:
+            return {
+                "id": None,
+                "nickname": "탈퇴한 유저",
+                "profile_img": None,
+            }
+        return {
+            "id": obj.sender.id,
+            "nickname": obj.sender.nickname,
+            "profile_img": obj.sender.profile_img,
+        }
