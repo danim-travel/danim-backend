2fdd69f69150855bd14905915dc9b2f815724b6a
Di-Mo-De-OH <dhelahep0115@gmail.com>
2026-06-12 14:07:20 +0900

    feat: kakao 소셜 로그인 모델 생성

--- apps/users/models/__init__.py
+++ apps/users/models/__init__.py
@@ -1,3 +1,4 @@
 from apps.users.models.models import User
-
-__all__ = ["User"]
+from apps.users.models.models import LoginType
+from apps.users.models.socialaccount import SocialAccount
+__all__ = ["User","LoginType","SocialAccount"]
