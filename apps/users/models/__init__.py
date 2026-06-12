4e706107206e51b04ffc7efb94a6a610c086e6f8
Di-Mo-De-OH <dhelahep0115@gmail.com>
2026-06-12 14:22:52 +0900

    feat: format 및 소셜 로그인 레디스 키 설정

--- apps/users/models/__init__.py
+++ apps/users/models/__init__.py
@@ -1,4 +1,4 @@
-from apps.users.models.models import User
-from apps.users.models.models import LoginType
+from apps.users.models.models import LoginType, User
 from apps.users.models.socialaccount import SocialAccount
-__all__ = ["User","LoginType","SocialAccount"]
+
+__all__ = ["User", "LoginType", "SocialAccount"]
