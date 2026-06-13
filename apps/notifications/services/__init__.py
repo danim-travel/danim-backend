a2bbabed75699b12e41556320d000b32312c9199
gunung-kim <unge001@naver.com>
2026-06-13 22:37:57 +0900

    feat : notifications 알림 개별 읽음 처리 api 서비스 작성

--- apps/notifications/services/__init__.py
+++ apps/notifications/services/__init__.py
@@ -1,3 +1,4 @@
 from apps.notifications.services.list_service import get_notification_list
+from apps.notifications.services.services import read_notification
 
-__all__ = ["get_notification_list"]
+__all__ = ["get_notification_list", "read_notification"]
---
+++ apps/notifications/services/services.py
@@ -0,0 +1,14 @@
+from apps.core.exceptions.exception import NotFoundException
+from apps.notifications.models import Notification
+
+
+def read_notification(notification_id, user):
+    notification = Notification.objects.filter(id=notification_id, receiver=user).update(
+        is_read=False
+    )
+    if not notification:
+        raise NotFoundException("해당 알림을 찾지 못했습니다.")
+
+    result = {"notification_id": notification_id, "is_read": True}
+
+    return result
