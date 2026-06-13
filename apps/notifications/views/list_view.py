9852237e9c476ea6417103055552c46e1ff53a68
gunung-kim <unge001@naver.com>
2026-06-13 23:30:15 +0900

    feat : notifications 전체 알림 읽음 처리 api view 임포트 추가

--- apps/notifications/views/list_view.py
+++ apps/notifications/views/list_view.py
@@ -6,7 +6,7 @@ from rest_framework.views import APIView
 from apps.core.utils.pagination import paginate
 from apps.notifications.schemas import notification_list_schema
 from apps.notifications.serializers import NotificationListSerializer
-from apps.notifications.services import get_notification_list
+from apps.notifications.services import get_notification_list, read_all_notifications
 
 
 class NotificationListView(APIView):
