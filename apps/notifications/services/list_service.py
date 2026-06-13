0d4f313566b4922cc01f4e68476a6eb15dffdf4e
gunung-kim <unge001@naver.com>
2026-06-13 23:29:33 +0900

    feat : notifications 전체 알림 읽음 처리 api service 작성

--- apps/notifications/services/list_service.py
+++ apps/notifications/services/list_service.py
@@ -4,3 +4,7 @@ from apps.notifications.models import Notification
 def get_notification_list(user):
     queryset = Notification.objects.filter(receiver=user).select_related("sender")
     return queryset
+
+
+def read_all_notifications(user):
+    Notification.objects.filter(receiver=user, is_read=False).update(is_read=True)
