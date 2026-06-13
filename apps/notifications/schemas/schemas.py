7b5b41ca37111e0f5c580c9c7671524992c4dc71
gunung-kim <unge001@naver.com>
2026-06-14 01:11:19 +0900

    docs : notification 알림 개별 삭제 api extend_schema 작성

--- apps/notifications/schemas/schemas.py
+++ apps/notifications/schemas/schemas.py
@@ -1,6 +1,9 @@
 from drf_spectacular.utils import OpenApiResponse, extend_schema
 
-from apps.notifications.serializers import NotificationReadSerializer
+from apps.notifications.serializers import (
+    NotificationDeleteSerializer,
+    NotificationReadSerializer,
+)
 
 notification_read_schema = extend_schema(
     responses={
@@ -10,4 +13,14 @@ notification_read_schema = extend_schema(
     },
     tags=["notifications"],
     summary="알림 개별 읽음 처리 api",
+)
+
+notification_delete_schema = extend_schema(
+    responses={
+        200: NotificationDeleteSerializer,
+        401: OpenApiResponse(description="로그인이 필요합니다."),
+        404: OpenApiResponse(description="해당 알림을 찾지 못했습니다."),
+    },
+    tags=["notifications"],
+    summary="알림 개별 삭제 처리 api",
 )
