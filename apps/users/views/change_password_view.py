1c5cf90cebadf1d52122e553fea13d577b731bdb
Di-Mo-De-OH <dhelahep0115@gmail.com>
2026-06-14 15:42:57 +0900

    docs: 스웨거 문서 추가

--- apps/users/views/change_password_view.py
+++ apps/users/views/change_password_view.py
@@ -7,6 +7,7 @@ from rest_framework.response import Response
 from rest_framework.views import APIView
 
 from apps.users.models import User
+from apps.users.schemas.change_password_schema import change_password_schema
 from apps.users.serializers.change_password_serializer import (
     ChangePasswordRequestSerializer,
 )
@@ -17,6 +18,7 @@ class ChangePasswordView(APIView):
     permission_classes = [IsAuthenticated]
     service = ChangePasswordService()
 
+    @change_password_schema
     def post(self, request: Request) -> Response:
         serializer = ChangePasswordRequestSerializer(data=request.data)
         serializer.is_valid(raise_exception=True)
