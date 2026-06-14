68c690ab1aefabd76a8c98fbfe3be9039b7cf01b
Di-Mo-De-OH <dhelahep0115@gmail.com>
2026-06-14 15:41:00 +0900

    feat: 비밀번호 변경 기능 구현

--- apps/users/validators.py
+++ apps/users/validators.py
@@ -10,3 +10,23 @@ def validate_nickname_format(value: str) -> str:
     if not re.match(r"^[a-zA-Z0-9_]+$", value):
         raise ValidationError("닉네임은 영문, 숫자, 언더바(_)만 사용 가능합니다.")
     return value
+
+
+def validate_password_format(value: str) -> str:
+    """
+    비밀번호는 8자리 이상
+    하나의 이상의 대문자 영어 포함
+    하나 이상의 숫자 포함
+    하나 이상의 특수문자 포함
+    """
+    if len(value) < 8:
+        raise ValidationError("비밀번호는 8자리 이상이어야 합니다.")
+    if not re.search(r"[A-Z]", value):
+        raise ValidationError("비밀번호는 하나 이상의 영문 대문자가 포함되어야 합니다.")
+    if not re.search(r"\d", value):
+        raise ValidationError("비밀번호는 하나 이상의 숫자가 포함되어야 합니다.")
+    if not re.search(r"[!@#$%^&*()]", value):
+        raise ValidationError(
+            "비밀번호는 적어도 하나 이상의 특수문자(!@#$%^&*())가 포함되어야 합니다."
+        )
+    return value
