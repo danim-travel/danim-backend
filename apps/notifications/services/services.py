51c4e4e2709703c096de47e1460e15867f7f3191
gunung-kim <unge001@naver.com>
2026-06-15 22:23:46 +0900

    fix : notification 알림 개별 읽음 처리 api service 코드 filter조건 추가 및 redis_cache 추가

--- apps/notifications/services/services.py
+++ apps/notifications/services/services.py
@@ -1,14 +1,25 @@
+from django.core.cache import cache
+
 from apps.core.exceptions.exception import NotFoundException
 from apps.notifications.models import Notification
 
 
 def read_notification(notification_id, user):
-    notification = Notification.objects.filter(id=notification_id, receiver=user).update(
-        is_read=True
-    )
+    notification = Notification.objects.filter(
+        id=notification_id, receiver=user, is_read=False
+    ).update(is_read=True)
     if not notification:
         raise NotFoundException("해당 알림을 찾지 못했습니다.")
 
     result = {"notification_id": notification_id, "is_read": True}
+
+    try:
+        cache.decr(f"user_{user.id}_unread_count")
+    except ValueError:
+        cache.set(
+            f"user_{user.id}_unread_count",
+            Notification.objects.filter(receiver=user, is_read=False).count(),
+            timeout=None,
+        )
 
     return result
