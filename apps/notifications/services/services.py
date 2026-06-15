0ea837da9b5938930acf56b852387811052d5746
gunung-kim <unge001@naver.com>
2026-06-16 00:19:01 +0900

    fix : notification 알림 개별 삭제 처레 api service reids_cache 추가

--- apps/notifications/services/services.py
+++ apps/notifications/services/services.py
@@ -18,10 +18,24 @@ def read_notification(notification_id, user):
 
 
 def delete_notification(notification_id, user):
+    is_unread = Notification.objects.filter(
+        id=notification_id, receiver=user, is_read=False
+    ).exists()
+
     notification, _ = Notification.objects.filter(
         id=notification_id, receiver=user
     ).delete()
     if not notification:
         raise NotFoundException("해당 알림을 찾지 못했습니다.")
+
+    if is_unread:
+        try:
+            cache.decr(f"user_{user.id}_unread_count")
+        except ValueError:
+            cache.set(
+                f"user_{user.id}_unread_count",
+                Notification.objects.filter(receiver=user, is_read=False).count(),
+                timeout=None,
+            )
 
     return {"notification_id": notification_id}
