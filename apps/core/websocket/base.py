8de254d7c135fe273d684a77601fe6d9d2d6db91
gunung-kim <unge001@naver.com>
2026-06-17 17:06:52 +0900

    feat : websocket baseconsumer 작성

---
+++ apps/core/websocket/base.py
@@ -0,0 +1,31 @@
+from channels.generic.websocket import AsyncWebsocketConsumer
+
+from apps.users.models import User
+
+
+class BaseConsumer(AsyncWebsocketConsumer):
+    """
+    websocket 사용 도메인의 공동 consumer
+    def connect -> scope에서 user 뽑고 None이면 close, 인증 성공 시 on_connect 호출
+    def on_connect -> 반드시 오버라이드 필요, 구현하지 않으면 NotImplementedError 발생
+    def disconnect -> 기존에 생성했던 group 삭제
+    """
+
+    user: User | None
+
+    async def connect(self):
+        self.user = self.scope.get("user")
+
+        if not self.user:
+            await self.close()
+            return
+
+        await self.accept()
+        await self.on_connect()
+
+    async def on_connect(self):
+        raise NotImplementedError
+
+    async def disconnect(self, close_code):
+        if hasattr(self, "group_name") and self.group_name:
+            await self.channel_layer.group_discard(self.group_name, self.channel_name)
