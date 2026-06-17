231a2f6b7da3c6da66ed5229d46dce1de3a826a1
gunung-kim <unge001@naver.com>
2026-06-17 20:37:43 +0900

    fix : key_view 임포트 오류수정

--- apps/core/websocket/websocket_key/view/key_view.py
+++ apps/core/websocket/websocket_key/view/key_view.py
@@ -1,6 +1,7 @@
+from rest_framework import status
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
-from rest_framework.views import APIView, status
+from rest_framework.views import APIView
 
 from apps.core.websocket.websocket_key.service import make_socket_key
 
