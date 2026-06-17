f938d2ad860a28688ffd1ad26a699d29eb764502
Di-Mo-De-OH <dhelahep0115@gmail.com>
2026-06-17 16:51:21 +0900

    docs: 스웨거 문서 추가

--- apps/posts/views/bookmark_list_view.py
+++ apps/posts/views/bookmark_list_view.py
@@ -4,6 +4,7 @@ from rest_framework.request import Request
 from rest_framework.response import Response
 from apps.posts.services.bookmark_list_service import BookmarkListService
 from apps.posts.serializers.bookmark_list_serializer import BookmarkListSerializer
+from apps.posts.schemas.bookmark_list_view_schema import bookmark_list_schema
 from apps.core.utils.pagination import  paginate
 from apps.users.models import User
 from typing import cast
@@ -13,6 +14,7 @@ class BookmarkListView(APIView):
     permission_classes = [IsAuthenticated]
     service = BookmarkListService()
 
+    @bookmark_list_schema
     def get(self,request:Request)->Response:
         queryset = self.service.get_bookmark_list(
             request_user=cast(User, request.user),
