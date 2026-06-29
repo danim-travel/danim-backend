0b3b197e3347088d318145dfdf5c2f215b798e4a
test <test@testui-MacBookPro.local>
2026-06-29 18:54:40 +0900

    fix : comment gif확장자를 위한 presignedview 및 serializer 수정 , COMMENT_ALLOWED_EXTENSIONS 작성

--- apps/core/storage/s3/constants.py
+++ apps/core/storage/s3/constants.py
@@ -4,3 +4,11 @@ ALLOWED_EXTENSIONS = {
     ".png": "image/png",
     ".webp": "image/webp",
 }
+
+COMMENT_ALLOWED_EXTENSIONS = {
+    ".jpg": "image/jpeg",
+    ".jpeg": "image/jpeg",
+    ".png": "image/png",
+    ".webp": "image/webp",
+    ".gif": "image/gif",
+}
